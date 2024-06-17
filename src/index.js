6cb7a1bfde1af907c3c00f670240846036565adf
Apoorva Jindal <jindalapoorva04@gmail.com>
2024-06-17 16:14:17 +0530

    Mongodb connection established

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import express from "express";
-import bodyParser from "body-parser";
-
-
 const app = express();
-const port = 3000;
+
+;( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/
+            ${DB_NAME}`);
+            app.on("error", (err) => {console.log(err); throw err});
+
+            app.listen(process.env.PORT, () => {
+                console.log(`Server is running on port: ${process.env.PORT}`);
+            });
+
+    } catch (err) {
+        console.error("ERROR: ", err);
+        throw err;
+    }
+})()
+*/
+ 
 
 
 
@@ -20,9 +59,6 @@ const port = 3000;
 
 
 
-
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is running on port: ${process.env.PORT}`);
+// });
