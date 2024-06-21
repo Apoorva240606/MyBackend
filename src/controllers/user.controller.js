918c397c2bc72f2bf1beb13839d928947742783d
Apoorva Jindal <jindalapoorva04@gmail.com>
2024-06-21 10:22:45 +0530

    add auth middleware and login and logout route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,24 @@ import {ApiError} from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import uploadOnCloudinary from '../utils/cloudinary.js';
 import { ApiResponse } from "../utils/ApiResponse.js";
+import fs from "fs";
+import cookie from "cookie";
 
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try{
+      const user =   await User.findById(userId)
+       const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+      await  user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    } catch(error){
+        throw new ApiError(500, "Something wnet wrong while generating refresh and access token")
+    }
+}
 
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
@@ -16,13 +33,15 @@ const registerUser = asyncHandler( async (req,res) => {
     // return res
 
     const  {fullName, email, username, password } = req.body;
-    console.log("email: ", email);
     
+    
+
     if( 
         [fullName, email, username , password].some((field) => 
             field?.trim() === ""
         )
     ){
+      
         throw new ApiError(400, "All fields are required")
     }
 
@@ -35,6 +54,8 @@ const registerUser = asyncHandler( async (req,res) => {
         throw new ApiError(409, "User already exists")
     }
 
+    console.log("hereeeeeee")
+    
    const avatarLocalPath = req.files?.avatar[0]?.path
     const coverImageLocalPath = req.files?.coverImage[0]?.path
 
@@ -58,7 +79,7 @@ const registerUser = asyncHandler( async (req,res) => {
   const user = await  User.create({
         fullName,
         avatar: avatar.url,
-        coverImage: coverImage.url,
+        coverImage: coverImage?.url || null,
         email,
         username: username.toLowerCase(),
         password,
@@ -79,6 +100,81 @@ const registerUser = asyncHandler( async (req,res) => {
 
 })
 
+const loginUser = asyncHandler( async (req,res) => {
+    //get user details
+    // username or email
+    //check that all required fields are not empty
+    //check that user exists
+    //check that password is correct
+    // access and refresh token
+    // send cookie
 
+    const { email, username, password } = req.body;
+    console.log("email",username)
+    if(!username){
+        throw new ApiError(400, "Email or username is required")
+    }
 
-export {registerUser}
+    const user =await User.findOne({
+        $or:[{email}, {username}]
+    })
+    console.log("user",user)
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+     console.log("Hwllldfdsf")
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid password")
+    }
+
+    const {accessToken, refreshToken} =await generateAccessTokenAndRefreshToken(user._id)
+
+   const loginUser = await User.findById(user._id).select("-password -refreshToken")
+   console.log("loginUser",loginUser)
+
+   const options = {
+    httpOnly:true,
+    secure: true
+   }
+
+   return res
+   .status(200)
+   .cookie("accessToken", accessToken, options)
+   .cookie("refreshToken", refreshToken, options)
+   .json(new ApiResponse(200, {
+                user: loginUser, accessToken, refreshToken
+                
+                 
+            }, "User logged in successfully"))
+
+})
+
+const logoutUser = asyncHandler( async (req,res) => {
+  await  User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly:true,
+        secure: true
+       }
+
+       return res
+       .status(200)
+       .clearCookie("accessToken", options)
+       .clearCookie("refreshToken", options)
+       .json(new ApiResponse(200, {}, "User logged out successfully"))
+
+})
+
+export {registerUser, loginUser, logoutUser}
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,12 +2,14 @@ import multer from 'multer';
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: (req, file, cb,) => {
         cb(null, "./public");
+        
     },
     filename: (req, file, cb) => {
         cb(null, `${file.originalname}-${Date.now()}`);
     }
+    
 })
 
 
